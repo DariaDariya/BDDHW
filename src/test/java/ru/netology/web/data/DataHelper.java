7f9ce9d0b3d86c89c444a886ce2317678d059e15
Daria Test <dashik_gem@icloud.com>
2023-04-26 23:02:44 +0300

    New commit

--- src/test/java/ru/netology/web/data/DataHelper.java
+++ src/test/java/ru/netology/web/data/DataHelper.java
@@ -2,6 +2,8 @@ package ru.netology.web.data;
 
 import lombok.Value;
 
+import java.util.Random;
+
 public class DataHelper {
     private DataHelper() {
     }
@@ -16,8 +18,24 @@ public class DataHelper {
         return new AuthInfo("vasya", "qwerty123");
     }
 
-    public static AuthInfo getOtherAuthInfo(AuthInfo original) {
+    public static AuthInfo getOtherAuthInfo() {
         return new AuthInfo("petya", "123qwerty");
+    }
+
+    @Value
+    public static class InfoCard {
+        private String cardNumber;
+        private String cardId;
+    }
+
+    public static InfoCard getFirstInfoCard() {
+        return new InfoCard("5559000000000001",
+                "92df3f1c-a033-48e6-8390-206f6b1f56c0");
+    }
+
+    public static InfoCard getSecondInfoCard() {
+        return new InfoCard("5559000000000002",
+                "0f3f5c2a-249e-4c3d-8287-09f7a039391d");
     }
 
     @Value
@@ -27,5 +45,17 @@ public class DataHelper {
 
     public static VerificationCode getVerificationCodeFor(AuthInfo authInfo) {
         return new VerificationCode("12345");
+    }
+
+    public static VerificationCode getOtherVerificationCodeFor(AuthInfo authInfo) {
+        return new VerificationCode("54321");
+    }
+
+    public static int generateValidAmount(int balance) {
+        return new Random().nextInt(balance) + 1;
+    }
+
+    public static int generateInvalidAmount(int balance) {
+        return Math.abs(balance) + new Random().nextInt(10000);
     }
 }
--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -1,22 +1,39 @@
 package ru.netology.web.test;
 
+import lombok.val;
 import org.junit.jupiter.api.Test;
 import ru.netology.web.data.DataHelper;
-import ru.netology.web.page.LoginPageV1;
+
 import ru.netology.web.page.LoginPageV2;
 
 import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static ru.netology.web.data.DataHelper.*;
 
 public class MoneyTransferTest {
     @Test
     void shouldTransferMoneyBetweenOwnCardsV1() {
         open("http://localhost:9999");
-        var loginPage =  new LoginPageV1();
+        var loginPage = new LoginPageV2();
         var authInfo = DataHelper.getAuthInfo();
         var verificationPage = loginPage.validLogin(authInfo);
         var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-        verificationPage.validVerify(verificationCode);
+        var DashboardPage = verificationPage.validVerify(verificationCode);
+        var infoCardFirst = DataHelper.getFirstInfoCard();
+        var infoCardSecond = DataHelper.getSecondInfoCard();
+        var firstDashboardPage = DashboardPage.getDashboardPage(infoCardFirst);
+        var secondDashboardPage = DashboardPage.getDashboardPage(infoCardSecond);
+        var amount = generateValidAmount(firstDashboardPage);
+        var expectedFirstDashboardPage = firstDashboardPage + amount;
+        var expectedSecondDashboardPage = secondDashboardPage - amount;
+        var transactionPage = DashboardPage.selectCardToTransfer(infoCardFirst);
+        transactionPage.validTransfer(String.valueOf(amount), infoCardSecond);
+
+        assertEquals(expectedFirstDashboardPage, DashboardPage.getDashboardPage(infoCardFirst));
+        assertEquals(expectedSecondDashboardPage, DashboardPage.getDashboardPage(infoCardSecond));
+
     }
+
     @Test
     void shouldTransferMoneyBetweenOwnCardsV2() {
         open("http://localhost:9999");
@@ -24,24 +41,42 @@ public class MoneyTransferTest {
         var authInfo = DataHelper.getAuthInfo();
         var verificationPage = loginPage.validLogin(authInfo);
         var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-        verificationPage.validVerify(verificationCode);
+        val DashboardPage = verificationPage.validVerify(verificationCode);
+        var infoCardSecond = DataHelper.getSecondInfoCard();
+        var infoCardFirst = DataHelper.getFirstInfoCard();
+        val secondDashboardPage = DashboardPage.getDashboardPage(infoCardSecond);
+        val firstDashboardPage = DashboardPage.getDashboardPage(infoCardFirst);
+        val amount = generateValidAmount(firstDashboardPage);
+        val expectedFirstDashboardPage = firstDashboardPage - amount;
+        val expectedSecondDashboardPage = secondDashboardPage + amount;
+        val transactionPage = DashboardPage.selectCardToTransfer(infoCardSecond);
+        transactionPage.validTransfer(String.valueOf(amount), infoCardFirst);
+
+        assertEquals(expectedSecondDashboardPage, DashboardPage.getDashboardPage(infoCardSecond));
+        assertEquals(expectedFirstDashboardPage, DashboardPage.getDashboardPage(infoCardFirst));
+
+    }
+
+    @Test
+    void shouldFailToAuthorizeWithInvalidVerificationCode() {
+        open("http://localhost:9999");
+        var loginPage = new LoginPageV2();
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var badVerificationCode = DataHelper.getOtherVerificationCodeFor(authInfo);
+        verificationPage.invalidVerify(badVerificationCode);
+    }
+
+    @Test
+    void shouldFailToAuthorizeWithInvalidVerificationCode2() {
+        open("http://localhost:9999");
+        var loginPage = new LoginPageV2();
+        var authInfo = DataHelper.getOtherAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var badVerificationCode = DataHelper.getOtherVerificationCodeFor(authInfo);
+        verificationPage.invalidVerify(badVerificationCode);
     }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
+
